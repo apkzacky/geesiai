5e8bc1429b21b079187d9adf95b6ba58ab63df6f
Zacky Jamel <128674980+apkzacky@users.noreply.github.com>
2024-07-11 21:28:59 +0300

    Add files via upload

--- src/auth.js
+++ src/auth.js
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google'
-import FacebookProvider from 'next-auth/providers/facebook'
 
 export const {
     handlers: { GET, POST },
